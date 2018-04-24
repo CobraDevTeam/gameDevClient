f88d6748aaeb70b9edd449de91e22970eb85581f
Antoine Cordelle <antoinecordelle@gmail.com>
2018-04-24 12:43:39 +0200

    Container comment

--- headers/gui/container.hpp
+++ headers/gui/container.hpp
@@ -7,16 +7,36 @@ namespace GUI {
 
 class Container : public Component
 {
+
+/**
+* @brief Racine du GUI contenant tous les autres composants
+*/
+
 public:
+    /**
+    * @brief Constructeur, initialise le GUI vide
+    */
+
     Container();
 
+    /**
+    * @brief Permet de rajouter au Container des Components
+    */
     void                            pack(Component::Ptr component);
 
+    /**
+    * @brief Détermine si le composant peut être selectionné ou pas : ici false
+    */
     virtual bool                    isSelectable() const;
+
+    /**
+     * @brief Prend en charge les interruptions et les traite : ici il les renvoie au composant selectionné
+     */
     virtual void                    handleEvent(const sf::Event &event);
 
 private:
     virtual void                    draw(sf::RenderTarget &target, sf::RenderStates states) const;
+
 
     std::vector<Component::Ptr>     mChildren;
 };
